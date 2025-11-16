b1b66ff1162d1ede5c8bf43634ade6ba2c192a3d
_arabica <54007341+PeriodGamingYT@users.noreply.github.com>
2025-11-16 00:48:11 -0700

    fix copy, increment version

--- js/copy.js
+++ js/copy.js
@@ -1,10 +1,12 @@
-const copyAreaElement = document.getElementById("copy-area")
+let copyAreaElement = null
 
 const copyText = (text) => {
 	if(navigator.clipboard != null) {
 		navigator.clipboard.writeText(text)
 		return
 	}
+
+	if(copyAreaElement == null) { return }
 
 	copyAreaElement.setAttribute("value", text)
 	copyAreaElement.style.display = "inherit"
@@ -14,6 +16,8 @@ const copyText = (text) => {
 }
 
 const hideCopyArea = () => {
+	if(copyAreaElement == null) { return }
+
 	const selection = document.getSelection()
 	selection.removeAllRanges()
 
@@ -22,7 +26,9 @@ const hideCopyArea = () => {
 	copyAreaElement.style.display = "none"
 }
 
-document.addEventListener("load", () => {
+window.addEventListener("load", () => {
+	copyAreaElement = document.getElementById("copy-area")
+
 	copyAreaElement.addEventListener("keydown", (event) => {
 		if(event.key == "Escape") { hideCopyArea() }
 	})
